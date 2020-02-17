59a798661a227c67a2ba791a776af1d0185c4fff
Juan Alberto Raya Rodríguez <jjrr1307@gmail.com>
2020-02-17 17:09:19 +0100

    simplifico buscar recursivo

--- Recursividad.java
+++ Recursividad.java
@@ -2,6 +2,7 @@ public class Recursividad {
 	
 	public static int buscar (int[] array, int dato){
 		int i=0;
+		System.out.print("* ");
 		while (i<array.length && dato!=array[i]){
 			System.out.print("* ");
 			i++;						
@@ -13,25 +14,22 @@ public class Recursividad {
 	}
 	
 	public static int buscarREC (int[] array, int dato, int i){	
-		System.out.print("* ");		
-		int pos = 0;
+		System.out.print("* ");
 		if (i==array.length) { //Caso llega al final sin encontrar
-			pos=-1;
+			return -1;
 		}else if (dato==array[i]) { //Caso encontrado
-			pos=i;						
-		}else {	//Caso recursivo		
-			i++;
-			pos=buscarREC(array, dato, i);
+			return i;						
+		}else {	//Caso recursivo	
+			return buscarREC(array, dato, i+1);
 		}
-		
-		return pos;		
+			
 	}
 
 	public static void main(String[] args) {
 		
 		int [] lista = {1,2,3,4,5,6,7,8,9,10};
-		System.out.println("Iterativa: " + buscar(lista,5));
-		System.out.println("Recursiva: " + buscarREC(lista,5,0));
+		System.out.println("Iterativa: " + buscar(lista,0));
+		System.out.println("Recursiva: " + buscarREC(lista,0,0));
 
 	}
 
