ea4596f9a557456c0ee2484334d32153fc8ccacc
Juan Alberto Raya Rodríguez <jjrr1307@gmail.com>
2020-02-24 17:15:39 +0100

    practica 24/2/20

---
+++ DyV.java
@@ -0,0 +1,24 @@
+
+public class DyV {
+	
+	 
+	public static int maximo (int[] lista, int inicio, int fin) {
+		int sol;
+		if (inicio == fin){
+			sol=lista[inicio];		
+		}else {
+			int mitad = (inicio + fin)/2;
+			int maxI = maximo(lista, inicio, mitad);
+			int maxD = maximo(lista, mitad+1, fin);
+			sol = Integer.max(maxI, maxD);
+		}
+		return sol;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] lista = {2,3,7,6,5,55,66,33,4};
+		System.out.println(maximo(lista,0,lista.length-1));
+	}
+
+}
