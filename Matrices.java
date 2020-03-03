081a361962741a1b33d064cafd9c1a79a8b89aa6
your name <jjrr1307@gmail.com>
2020-03-03 23:37:53 +0100

    completado

--- Matrices.java
+++ Matrices.java
@@ -30,6 +30,26 @@ public class Matrices {
 		}
 	}
 
+	public static boolean sonIgualesDyV2(int[][] m1, int[][] m2, int iniy, int finy) {
+
+		boolean valor = true;
+		if (iniy == finy) {
+			int i = 0;
+			while (i < m1.length ) {
+				if (m1[i][iniy] != m2[i][iniy]) {
+					valor = false;
+				}
+				i++;
+			}
+		} else {
+			return sonIgualesDyV2(m1, m2, iniy, (finy + iniy) / 2)
+					&& sonIgualesDyV2(m1, m2, (finy + iniy) / 2 + 1, finy);
+
+		}
+		return valor;
+
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[][] m1 = { { 1, 2, 3, 4, 5, 6, 7, 8 }, { 1, 2, 3, 4, 5, 6, 7, 8 }, { 1, 2, 3, 4, 5, 6, 7, 8 },
@@ -39,9 +59,12 @@ public class Matrices {
 				{ 1, 2, 3, 4, 5, 6, 7, 8 }, { 1, 2, 3, 4, 5, 6, 7, 8 }, { 1, 2, 3, 4, 5, 6, 7, 8 },
 				{ 1, 2, 3, 4, 5, 6, 7, 8 }, { 1, 2, 3, 4, 5, 6, 7, 8 } };
 
-		System.out.println("Comparación iterativa: " + sonIguales(m1, m2));
-		System.out.println("Comparación DyV entre 4: " + sonIgualesDyV4(m1, m2, 0, 7, 0, 7));
-//		System.out.println("Comparación DyV entre 2: "+sonIgualesDyV2(m1,m2));
+//		int[][] m1 = { { 1, 2, 3, 4 }, { 1, 2, 3, 4 }, { 1, 2, 3, 4 }, { 1, 2, 3, 4 } };
+//		int[][] m2 = { { 1, 2, 3, 4 }, { 1, 2, 3, 4 }, { 1, 2, 3, 4 }, { 1, 2, 3, 4 } };
+
+		System.out.println("Comparacion iterativa: " + sonIguales(m1, m2));
+		System.out.println("Comparacion DyV entre 4: " + sonIgualesDyV4(m1, m2, 0, 7, 0, 7));
+		System.out.println("Comparacion DyV entre 2: " + sonIgualesDyV2(m1, m2, 0, 7));
 
 	}
 
