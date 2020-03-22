b8af929149460d47d14adf7663a0c0c21e643608
Juan Alberto Raya <jjrr1307@gmail.com>
2020-03-22 15:41:25 +0100

    hecho el de seleccion, dudas en el mergesort

--- src/AlgoritmosOrdenacion.java
+++ src/AlgoritmosOrdenacion.java
@@ -1,6 +1,29 @@
 
 public class AlgoritmosOrdenacion {
 
+	public static void mergesort(int[] lista, int inicio, int fin) {
+		if (inicio < fin) {
+			int med = (inicio + fin) / 2;
+//			int[] lizq = {};
+//			int[] lder = {};
+//
+//			for (int i = 0; i < med; i++) {
+//				lizq[i] = lista[i];
+//			}
+//			for (int i = med + 1; med + 1 < fin; i++) {
+//				lder[i] = lista[i];
+//			}
+			mergesort(lista, inicio, med);
+			mergesort(lista, med + 1, fin);
+//			merge();
+		}
+
+	}
+
+	public static void merge(int[] lista, int inicio, int fin) {
+
+	}
+
 	public static void quicksort(int[] lista, int inicio, int fin) {
 
 		if (inicio < fin) {
@@ -13,7 +36,8 @@ public class AlgoritmosOrdenacion {
 
 	}
 
-	public static int pivotar(int[] lista, int inicio, int fin) {
+	public static int pivotar(int[] lista, int inicio, int fin) { // coge el 1er elemento del array y lo ubica en su
+																	// posicion final, devuelve su posicion
 
 		int i = inicio;
 		int p = lista[inicio];
@@ -36,8 +60,23 @@ public class AlgoritmosOrdenacion {
 		return i;
 	}
 
+	public static int seleccion(int[] lista, int k) { // localiza el elemento i-esimo
+		int ini = 0;
+		int fin = lista.length - 1;
+		int x = pivotar(lista, ini, fin);
+		while (k != x) {
+			if (x > k) {
+				fin = x - 1;
+			} else {
+				ini = x + 1;
+			}
+			x = pivotar(lista, ini, fin);
+		}
+		return lista[x];
+	}
+
 	public static void main(String[] args) {
-		int[] lista = { 3, 3, 2, 2 };
+		int[] lista = { 4, 3, 1, 7, 2 };
 		for (int i = 0; i < lista.length; i++) {
 			System.out.print(lista[i] + ", ");
 		}
@@ -47,6 +86,9 @@ public class AlgoritmosOrdenacion {
 		for (int i = 0; i < lista.length; i++) {
 			System.out.print(lista[i] + ", ");
 		}
+		System.out.println();
+		System.out.println("**********************************");
+		System.out.println("El elemento de la lista situado en la posicion dada es el: " + seleccion(lista, 0));
 
 	}
 
