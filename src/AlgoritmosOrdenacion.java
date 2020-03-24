f8216078913f10481071f3c36a14477937e0f475
Juan Alberto Raya <jjrr1307@gmail.com>
2020-03-24 13:19:55 +0100

    mergesort buscada en google

--- src/AlgoritmosOrdenacion.java
+++ src/AlgoritmosOrdenacion.java
@@ -1,28 +1,81 @@
 
 public class AlgoritmosOrdenacion {
 
-	public static void mergesort(int[] lista, int inicio, int fin) {
-		if (inicio < fin) {
-			int med = (inicio + fin) / 2;
-//			int[] lizq = {};
-//			int[] lder = {};
-//
-//			for (int i = 0; i < med; i++) {
-//				lizq[i] = lista[i];
-//			}
-//			for (int i = med + 1; med + 1 < fin; i++) {
-//				lder[i] = lista[i];
-//			}
-			mergesort(lista, inicio, med);
-			mergesort(lista, med + 1, fin);
-//			merge();
-		}
-
-	}
-
-	public static void merge(int[] lista, int inicio, int fin) {
-
-	}
+	 // Merges two subarrays of arr[]. 
+    // First subarray is arr[l..m] 
+    // Second subarray is arr[m+1..r] 
+    public static void merge(int arr[], int l, int m, int r) 
+    { 
+        // Find sizes of two subarrays to be merged 
+        int n1 = m - l + 1; 
+        int n2 = r - m; 
+  
+        /* Create temp arrays */
+        int L[] = new int [n1]; 
+        int R[] = new int [n2]; 
+  
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i) 
+            L[i] = arr[l + i]; 
+        for (int j=0; j<n2; ++j) 
+            R[j] = arr[m + 1+ j];   
+  
+        /* Merge the temp arrays */
+  
+        // Initial indexes of first and second subarrays 
+        int i = 0, j = 0; 
+  
+        // Initial index of merged subarry array 
+        int k = l; 
+        while (i < n1 && j < n2) 
+        { 
+            if (L[i] <= R[j]) 
+            { 
+                arr[k] = L[i]; 
+                i++; 
+            } 
+            else
+            { 
+                arr[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        } 
+  
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+  
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) 
+        { 
+            arr[k] = R[j]; 
+            j++; 
+            k++; 
+        } 
+    } 
+  
+    // Main function that sorts arr[l..r] using 
+    // merge() 
+    public static void mergesort(int arr[], int l, int r) 
+    { 
+        if (l < r) 
+        { 
+            // Find the middle point 
+            int m = (l+r)/2; 
+  
+            // Sort first and second halves 
+            mergesort(arr, l, m); 
+            mergesort(arr , m+1, r); 
+  
+            // Merge the sorted halves 
+            merge(arr, l, m, r); 
+        } 
+    } 
 
 	public static void quicksort(int[] lista, int inicio, int fin) {
 
@@ -82,6 +135,14 @@ public class AlgoritmosOrdenacion {
 		}
 		System.out.println();
 		System.out.println("**********************************");
+		
+		mergesort(lista, 0, lista.length - 1);
+		for (int i = 0; i < lista.length; i++) {
+			System.out.print(lista[i] + ", ");
+		}
+		System.out.println();
+		System.out.println("**********************************");		
+		
 		quicksort(lista, 0, lista.length - 1);
 		for (int i = 0; i < lista.length; i++) {
 			System.out.print(lista[i] + ", ");
