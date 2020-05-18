8cb36ed2e7451cb211669b1c00428f43e0c8236c
Juan Alberto Raya <jjrr1307@gmail.com>
2020-05-18 21:16:35 +0200

    CambioMinimo2

--- src/FastFoward/CambioMinimo.java
+++ src/FastFoward/CambioMinimo.java
@@ -9,7 +9,6 @@ public class CambioMinimo {
         int devolver = -1;
         int i = 0;
         boolean condicion = true;
-
         while (condicion && i < monedas.length) {
             if (acumulado.add(monedas[i]).compareTo(cambio) == -1 || acumulado.add(monedas[i]).compareTo(cambio) == 0 ) {
                 condicion = false;
@@ -28,7 +27,6 @@ public class CambioMinimo {
         BigDecimal acumulado = new BigDecimal("0");
         int posMoneda = 0;
         boolean haySol = true;
-
         while (!(acumulado.compareTo(cambio)==0) && haySol) {
             posMoneda = seleccionarMoneda(monedas, acumulado, cambio);
             if (posMoneda == -1) {
@@ -43,17 +41,23 @@ public class CambioMinimo {
         return haySol;
     }//cambioMin
 
-    //terminar la funcion y calcular complejidad
+//    n=parte entera del cambio
+//    La complejidad es O(n) = n/2+7 es decir n/2
 
     public static void main(String[] args) {
 
         BigDecimal[] monedas = {new BigDecimal("2"), new BigDecimal("1"), new BigDecimal("0.5"), new BigDecimal("0.2"), new BigDecimal("0.1"), new BigDecimal("0.05"), new BigDecimal("0.02"),new BigDecimal("0.01")};
         BigDecimal cambio = new BigDecimal("5.89");
         int[] solucion = {0, 0, 0, 0, 0, 0, 0, 0};
+        int num=0;
         System.out.println(cambioMin(monedas, cambio, solucion));
         for (int i = 0; i < solucion.length; i++) {
             System.out.print(solucion[i] + " ,");
+            num+=solucion[i];
+
         }
+        System.out.println();
+        System.out.println("Número de monedas: "+num);
 
     }
 }
